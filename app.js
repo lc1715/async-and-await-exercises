9990be114802080fbfe0e79af05d77a925330613
Lisa Chan <lc131715@gmail.com>
2024-03-02 18:27:00 -0800

    add Part 2 Question 2

--- app.js
+++ app.js
@@ -89,7 +89,28 @@ async function singleCard() {
 
 singleCard()
 
+// 2. Make a request to the deck of cards API to request a single card from a newly shuffled deck. Once you have the card, make a request to the same API to get one more card from the same deck.
 
+
+async function drawCards() {
+    try {
+        let resp = await axios.get('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+        let deckId = resp.data.deck_id
+        let cardResp1 = await axios.get(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+        let cardResp2 = await axios.get(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+
+        let { suit, value } = cardResp1.data.cards[0]
+        console.log(`${value} of ${suit}`)
+
+        let suit2 = cardResp2.data.cards[0].suit
+        let value2 = cardResp2.data.cards[0].value
+        console.log(`${value2} of ${suit2}`)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+drawCards()
 
 // 3. Build an HTML page that lets you draw cards from a deck. When the page loads, go to the Deck of Cards API to create a new deck, and show a button on the page that will let you draw a card. Every time you click the button, display a new card, until there are no cards left in the deck.'
 // https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1
